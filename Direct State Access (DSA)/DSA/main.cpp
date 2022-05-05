75393603fcd617d9e2fab61ba358824e8885cc92
Julian <julianstamb@gmail.com>
2022-05-05 13:02:51 +0200

    fix minification filter where mipmaps are used

--- Direct State Access (DSA)/DSA/main.cpp
+++ Direct State Access (DSA)/DSA/main.cpp
@@ -137,7 +137,7 @@ int main()
 	GLuint tex;
 	glCreateTextures(GL_TEXTURE_2D, 1, &tex);
 
-	glTextureParameteri(tex, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTextureParameteri(tex, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
 	glTextureParameteri(tex, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 	glTextureParameteri(tex, GL_TEXTURE_WRAP_S, GL_REPEAT);
 	glTextureParameteri(tex, GL_TEXTURE_WRAP_T, GL_REPEAT);
